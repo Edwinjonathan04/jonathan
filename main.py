6e316e6d5f83a77a63875e2050aa7eb09ab86345
Edwinjonathan04 <jonathan.edwin@binghamuni.edu.ng>
2023-11-12 22:19:00 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+p = float(input("enter p :"))
+
+r = float(input("enter r :"))
+
+t = float(input("enter t :"))
+
+n = float(input("enter n :"))
+
+SI = str(p * (1 + r * t))
+CI = p * (1 + r/n) ** n * t
+
+print("yusuf and sons " + SI)
+
